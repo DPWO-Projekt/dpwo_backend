0649aa1f78ad84dc8e581acd88c9c41fdb7c939d
pawelprokopiuk <99580002+pawelprokopiuk@users.noreply.github.com>
2025-06-04 21:02:49 +0200

    show owned dataset list

--- src/main/java/com/example/dpwo_backend/dataset/repository/DatasetRepository.java
+++ src/main/java/com/example/dpwo_backend/dataset/repository/DatasetRepository.java
@@ -2,6 +2,11 @@ package com.example.dpwo_backend.dataset.repository;
 
 import com.example.dpwo_backend.dataset.model.Dataset;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DatasetRepository extends MongoRepository<Dataset, String> {
+    List<Dataset> findByOwnerId(String ownerId);
 }
