082b7b5cd22de66f5c6f87402cace65a8b618dc8
Daniel-Stankowski <daniostan909v@gmail.com>
2025-06-05 00:20:35 +0200

    Null fix

--- src/main/java/com/example/dpwo_backend/dataset/dto/DatasetRequest.java
+++ src/main/java/com/example/dpwo_backend/dataset/dto/DatasetRequest.java
@@ -6,6 +6,7 @@ import com.example.dpwo_backend.dataset.dto.vcard.VCardRequest;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -28,6 +29,7 @@ public class DatasetRequest {
     @JsonProperty("vCard")
     private VCardRequest vCard;
 
+    @NotNull(message = "Dataset distribution is required")
     private List<DatasetDistributionRequest> datasetDistributions;
     @JsonProperty("parentCatalog")
     private String parentCatalog;
